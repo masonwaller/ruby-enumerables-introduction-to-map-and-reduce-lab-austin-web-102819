f09ff9f011a431c2e1aeb5138e41d10f0d91697c
Mason Waller <masonwaller805@gmail.com>
2019-10-09 20:01:18 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -55,7 +55,7 @@ def reduce_to_all_true(source_array)
   end
   return true
 end
-def reduce_to_all_true(source_array)
+def reduce_to_any_true(source_array)
   i=0 
   while i<source_array.length do
     return true if !!source_array[i]
